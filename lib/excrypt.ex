2feef6b3799065b94a5bbce9ff95be785b430b18
Koni <74613145+vKxni@users.noreply.github.com>
2022-03-29 18:55:27 +0200

    clean up

--- lib/excrypt.ex
+++ lib/excrypt.ex
@@ -1,17 +1,4 @@
 defmodule Excrypt do
-  @moduledoc """
-  Documentation for `Excrypt`.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Excrypt.hello()
-      :world
-
-  """
   def hello do
     :world
   end
